e44e2bbb019dd094e51bb35306266bc650893d0f
teraJumper98193 <tomasz.swider@gmail.com>
2016-03-29 00:33:23 -0400

    Assignment 22 completed

--- models/Address_book.rb
+++ models/Address_book.rb
@@ -17,4 +17,45 @@ require_relative 'entry'
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
    end
- end
+
+   # Search AddressBook for a specific entry by name
+   def binary_search(name)
+     # #1
+     lower = 0
+     upper = entries.length - 1
+
+     # #2
+       while lower <= upper
+         # #3
+         mid = (lower + upper) / 2
+         mid_name = entries[mid].name
+
+         # #4
+         if name == mid_name
+           return entries[mid]
+         elsif name < mid_name
+           upper = mid - 1
+         elsif name > mid_name
+           lower = mid + 1
+         end
+       end
+
+       # #5
+       return nil
+   end
+
+   def iterative_search(name, array)
+  i = 0
+
+  while i <= (array.length - 1)
+    if array[i] == name
+      return array[i]
+    else
+      i += 1
+    end
+  end
+
+  return nil
+
+  end
+end
